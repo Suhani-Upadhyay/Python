b92afd6a5558fd97542f4d7ae856b9ba4a91d78d
Suhani Upadhyay <113186758+Suhani-Upadhyay@users.noreply.github.com>
2022-09-21 11:11:36 +0530

    Add files via upload

---
+++ basic python.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world!")
+# ()--->small backets
+
+
+# In[2]:
+
+
+print('hello world!')
+
+
+# In[3]:
+
+
+# ('single quote') and ("double quote")
+
+
+# In[4]:
+
+
+print('ram's book' )
+# invalid syntax--- likha(logic) shi hai pr syntax glt hai
+# we can't use " inside '
+
+
+# In[1]:
+
+
+print("ram's book")
+
+
+# In[6]:
+
+
+print('''hello
+world''')
+
+
+# In[7]:
+
+
+print("""hello
+world""")
+
+
+# In[3]:
+
+
+# ''' '''  or """ """(double triple quote)-----> use for multiple lines
+
+
+# In[6]:
+
+
+print('123')
+# string
+
+
+# In[7]:
+
+
+print(123)
+# number
+
+
+# In[ ]:
+
+
+
+
